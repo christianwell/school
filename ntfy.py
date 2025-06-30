79f7519ac4ecd99d6120f85c21c50f7bcf14ea4b
christianwell <94742270+christianwell@users.noreply.github.com>
2025-06-30 20:27:17 +0200

    Update ntfy.py
    added a weather api

--- ntfy.py
+++ ntfy.py
@@ -1,22 +1,18 @@
 import requests# gører så jeg kan send ting til ntfy.sh
 import feedparser# gører så jeg kan læse rss feeds
-import time
-from datetime import datetime
-
+linkvejr = "https://api.open-meteo.com/v1/forecast?latitude=55.1964&longitude=10.251&daily=weather_code,temperature_2m_max,apparent_temperature_max,temperature_2m_min,apparent_temperature_min,daylight_duration,sunset,sunrise&current_weather=true&timezone=auto&forecast_days=1&temporal_resolution=native&models=dmi_seamless"
+response = requests.get(linkvejr)
+data = response.json()  
+nuvejr = data["current_weather"]
+nuvejr = nuvejr["temperature"]
 d = feedparser.parse("https://www.dr.dk/nyheder/service/feeds/senestenyt")
 entry = d["entries"][0]
 nyhed1 = entry["title"]
 linknyhed = entry["link"]
 kanye = requests.get("https://api.kanye.rest/").json()["quote"]
-tid = "07:00"
-ntfy = "https://ntfy.sh/your_topic"  # dit nfty topic
-text = f'Godmorgen, dagens sidste nyt er: ✏️ {nyhed1} ({linknyhed})\nKanye quote af i dag: "{kanye}"'
+ntfy = "https://ntfy.sh/test101020"  # dit nfty topic
+text = f'Godmorgen, dagens sidste nyt er: ✏️ {nyhed1} ({linknyhed})\nKanye quote af i dag: "{kanye} " idags vejr er {nuvejr}°C. Husk at tage tøj på efter vejret! ☀️🌧️"'
 
-while True:
-    current_time = datetime.now().strftime("%H:%M")
-    if current_time == tid:
-        requests.post(ntfy, data=text.encode('utf-8'))
-        break
-    time.sleep(30)
+requests.post(ntfy, data=text.encode('utf-8'))
 
     
