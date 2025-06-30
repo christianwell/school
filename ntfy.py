417da9d8d489bf3aa505ef4122598ef334f4c4ca
christianwell <94742270+christianwell@users.noreply.github.com>
2025-06-30 20:29:26 +0200

    Update ntfy.py

--- ntfy.py
+++ ntfy.py
@@ -1,6 +1,6 @@
 import requests# gører så jeg kan send ting til ntfy.sh
 import feedparser# gører så jeg kan læse rss feeds
-linkvejr = "https://api.open-meteo.com/v1/forecast?latitude=55.1964&longitude=10.251&daily=weather_code,temperature_2m_max,apparent_temperature_max,temperature_2m_min,apparent_temperature_min,daylight_duration,sunset,sunrise&current_weather=true&timezone=auto&forecast_days=1&temporal_resolution=native&models=dmi_seamless"
+linkvejr = "https://api.open-meteo.com/v1,"
 response = requests.get(linkvejr)
 data = response.json()  
 nuvejr = data["current_weather"]
@@ -10,7 +10,7 @@ entry = d["entries"][0]
 nyhed1 = entry["title"]
 linknyhed = entry["link"]
 kanye = requests.get("https://api.kanye.rest/").json()["quote"]
-ntfy = "https://ntfy.sh/test101020"  # dit nfty topic
+ntfy = "https://ntfy.sh/urntfy"  # dit nfty topic
 text = f'Godmorgen, dagens sidste nyt er: ✏️ {nyhed1} ({linknyhed})\nKanye quote af i dag: "{kanye} " idags vejr er {nuvejr}°C. Husk at tage tøj på efter vejret! ☀️🌧️"'
 
 requests.post(ntfy, data=text.encode('utf-8'))
