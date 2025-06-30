757ea053974c6e7b2c13e4580fe3e2476cc410ed
christianwell <christiandubsky20@gmail.com>
2025-06-30 18:51:05 +0200

    Save local changes before rebase

--- ntfy.py
+++ ntfy.py
@@ -1,15 +1,22 @@
-import requests # gører så jeg kan send ting til ntfy.sh
-import feedparser # gører så jeg kan læse rss feeds
+import requests# gører så jeg kan send ting til ntfy.sh
+import feedparser# gører så jeg kan læse rss feeds
 import time
+from datetime import datetime
+
 d = feedparser.parse("https://www.dr.dk/nyheder/service/feeds/senestenyt")
-entry=d["entries"][1]
-nyhed1 = entry["title"] 
+entry = d["entries"][0]
+nyhed1 = entry["title"]
 linknyhed = entry["link"]
 kanye = requests.get("https://api.kanye.rest/").json()["quote"]
+tid = "07:00"
+ntfy = "https://ntfy.sh/your_topic"  # dit nfty topic
+text = f'Godmorgen, dagens sidste nyt er: ✏️ {nyhed1} ({linknyhed})\nKanye quote af i dag: "{kanye}"'
 
+while True:
+    current_time = datetime.now().strftime("%H:%M")
+    if current_time == tid:
+        requests.post("", data=text.encode('utf-8'))
+        break
+    time.sleep(30)
 
-text = f'Godmorgen , dagens sidste nyt er: ✏️ {linknyhed}{nyhed1} : Kanye quote af i dag: {kanye}'
-
-
-requests.post("https://ntfy.sh/", 
-    text.encode(encoding='utf-8'))
+    
